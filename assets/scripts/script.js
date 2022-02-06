b4bd2e4e9fc2ea0fa206fdac8130195774f53bc8
eirikstrompedersen <eirikstrompedersen@gmail.com>
2022-02-06 23:11:51 +0000

    Tidy up css and js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,341 +1,274 @@
 /*  https://www.welovequizzes.com/countries-of-the-world-quiz-questions-and-answers/ */
 
 
-/* List of questions */ 
+/* List of questions */
 
 let questionList = [{
-        question: 'What is the smallest country in the world with an area of only 0.49 square kilometres?',
-        alternatives: ['Monaco', 'Vatican', 'Andorra'],
-        answer: 1
-    },
+    question: 'What is the smallest country in the world with an area of only 0.49 square kilometres?',
+    alternatives: ['Monaco', 'Vatican', 'Andorra'],
+    answer: 1
+},
 
-    {
-        question: 'What is the second-largest country in the world after Russia?',
-        alternatives: ['China', 'USA', 'Canada'],
-        answer: 2
-    },
+{
+    question: 'What is the second-largest country in the world after Russia?',
+    alternatives: ['China', 'USA', 'Canada'],
+    answer: 2
+},
 
-    {
-        question: 'Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?',
-        alternatives: ['Kazakhstan', 'Madagascar', 'Turkey'],
-        answer: 0
-    },
+{
+    question: 'Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?',
+    alternatives: ['Kazakhstan', 'Madagascar', 'Turkey'],
+    answer: 0
+},
 
-    {
-        question: 'Which of the following countries is not in Central America?',
-        alternatives: ['Cuba', 'Mexic', 'Uruguay'],
-        answer: 2
-    },
+{
+    question: 'Which of the following countries is not in Central America?',
+    alternatives: ['Cuba', 'Mexic', 'Uruguay'],
+    answer: 2
+},
 
-    {
-        question: 'Which of the following countries has the largest Muslim population in the world?',
-        alternatives: ['Saudi Arabia', 'Indonesia', 'Iran'],
-        answer: 1
-    },
+{
+    question: 'Which of the following countries has the largest Muslim population in the world?',
+    alternatives: ['Saudi Arabia', 'Indonesia', 'Iran'],
+    answer: 1
+},
 
-    {
-        question: 'Which European country is divided into administrative divisions called departments?',
-        alternatives: ['France', 'Spain', 'Portugal'],
-        answer: 0
-    },
+{
+    question: 'Which European country is divided into administrative divisions called departments?',
+    alternatives: ['France', 'Spain', 'Portugal'],
+    answer: 0
+},
 
-    {
-        question: 'Which of the following countries is shaped like a high heel boot?',
-        alternatives: ['Chile', 'Finland', 'Italy'],
-        answer: 2
-    },
+{
+    question: 'Which of the following countries is shaped like a high heel boot?',
+    alternatives: ['Chile', 'Finland', 'Italy'],
+    answer: 2
+},
 
-    {
-        question: 'Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?',
-        alternatives: ['Kenya', 'Tanzania', 'Lesotho'],
-        answer: 1
-    },
+{
+    question: 'Mouth Kilimanjaro, the highest mountain in Africa, is located in which country?',
+    alternatives: ['Kenya', 'Tanzania', 'Lesotho'],
+    answer: 1
+},
 
-    {
-        question: 'In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?',
-        alternatives: ['Guatemala', 'Peru', 'Bolivia'],
-        answer: 1
-    },
+{
+    question: 'In which country can you visit Machu Picchu, which used to be the citadel of the Inca Empire?',
+    alternatives: ['Guatemala', 'Peru', 'Bolivia'],
+    answer: 1
+},
 
-    {
-        question: 'Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?',
-        alternatives: ['Cambodia', 'Vietnam', 'Malaysia'],
-        answer: 1
-    },
+{
+    question: 'Pho, which contains beef, herbs, rice noodles, and broth, is a traditional dish in which Asian country?',
+    alternatives: ['Cambodia', 'Vietnam', 'Malaysia'],
+    answer: 1
+},
 
-    {
-        question: 'Which of the following countries is often regarded as the oldest republic in the world?',
-        alternatives: ['Greece', 'Italy', 'San Marino'],
-        answer: 2
-    },
-    {
-        question: 'Which country will host the 2024 Olympics Summer Games?',
-        alternatives: ['France', 'Australia', 'Japan'],
-        answer: 0
-    },
+{
+    question: 'Which of the following countries is often regarded as the oldest republic in the world?',
+    alternatives: ['Greece', 'Italy', 'San Marino'],
+    answer: 2
+},
+{
+    question: 'Which country will host the 2024 Olympics Summer Games?',
+    alternatives: ['France', 'Australia', 'Japan'],
+    answer: 0
+},
 
-    {
-        question: 'Which country has the most islands in the world with more than 221,800?',
-        alternatives: ['Canada', 'Norway', 'Sweden'],
-        Correctanswer: 2
-    },
+{
+    question: 'Which country has the most islands in the world with more than 221,800?',
+    alternatives: ['Canada', 'Norway', 'Sweden'],
+    Correctanswer: 2
+},
 
-    {
-        question: 'Which country is often called Land of Rising Sun?',
-        alternatives: ['China', 'Japan', 'South Korea'],
-        answer: 1
-    },
+{
+    question: 'Which country is often called Land of Rising Sun?',
+    alternatives: ['China', 'Japan', 'South Korea'],
+    answer: 1
+},
 
-    {
-        question: 'On 15 August 1947, British India was separated into India and which modern-day country?',
-        alternatives: ['Pakistan', 'Bangladesh', 'Nepal'],
-        answer: 0
-    },
+{
+    question: 'On 15 August 1947, British India was separated into India and which modern-day country?',
+    alternatives: ['Pakistan', 'Bangladesh', 'Nepal'],
+    answer: 0
+},
 
-    {
-        question: 'Which country in eastern Europe used to be known as White Russia?',
-        alternatives: ['Ukraina', 'Belarus', 'Bulgaria'],
-        answer: 1
-    },
+{
+    question: 'Which country in eastern Europe used to be known as White Russia?',
+    alternatives: ['Ukraina', 'Belarus', 'Bulgaria'],
+    answer: 1
+},
 
-    {
-        question: 'Kimono is the national dress of which country?',
-        alternatives: ['South Korea', 'China', 'Japan'],
-        answer: 2
-    },
+{
+    question: 'Kimono is the national dress of which country?',
+    alternatives: ['South Korea', 'China', 'Japan'],
+    answer: 2
+},
 
-    {
-        question: 'Guitar is originated from which country in the 16th century?',
-        alternatives: ['Greece', 'France', 'Spain'],
-        answer: 2
-    },
+{
+    question: 'Guitar is originated from which country in the 16th century?',
+    alternatives: ['Greece', 'France', 'Spain'],
+    answer: 2
+},
 
-    {
-        question: 'What is the most populous country in the European Union?',
-        alternatives: ['Russia', 'Germany', 'France'],
-        answer: 1
-    },
+{
+    question: 'What is the most populous country in the European Union?',
+    alternatives: ['Russia', 'Germany', 'France'],
+    answer: 1
+},
 
-    {
-        question: 'Which two nations share the longest border in the world?',
-        alternatives: ['USA and Canada', 'China and Russia', 'Brazil and Argentina'],
-        answer: 0
-    },
+{
+    question: 'Which two nations share the longest border in the world?',
+    alternatives: ['USA and Canada', 'China and Russia', 'Brazil and Argentina'],
+    answer: 0
+},
 
-    {
-        question: 'Which of the following island countries is located on the Pacific Ocean?',
-        alternatives: ['Tuvalu', 'Jamaica', 'Malta'],
-        answer: 0
-    },
-    {
-        question: 'Dirham is the official currency of which country?',
-        alternatives: ['Iraq', 'Egypt', 'United Arab Emirates'],
-        answer: 2
-    },
+{
+    question: 'Which of the following island countries is located on the Pacific Ocean?',
+    alternatives: ['Tuvalu', 'Jamaica', 'Malta'],
+    answer: 0
+},
+{
+    question: 'Dirham is the official currency of which country?',
+    alternatives: ['Iraq', 'Egypt', 'United Arab Emirates'],
+    answer: 2
+},
 
-    {
-        question: 'The unicorn is the official national animal of which country?',
-        alternatives: ['Wales', 'Northern Ireland', 'Scotland'],
-        answer: 2 
-    },
+{
+    question: 'The unicorn is the official national animal of which country?',
+    alternatives: ['Wales', 'Northern Ireland', 'Scotland'],
+    answer: 2
+},
 
-    {
-        question: 'Which of the following countries is not a Nordic country?',
-        alternatives: ['Iceland', 'Russia', 'Denmark'],
-        answer: 1
-    },
+{
+    question: 'Which of the following countries is not a Nordic country?',
+    alternatives: ['Iceland', 'Russia', 'Denmark'],
+    answer: 1
+},
 
-    {
-        question: 'What is the only country in the world that has a national flag that is not rectangular?',
-        alternatives: ['Nepal', 'Switzerland', 'Kyrgyzstan'],
-        answer: 0
-    },
+{
+    question: 'What is the only country in the world that has a national flag that is not rectangular?',
+    alternatives: ['Nepal', 'Switzerland', 'Kyrgyzstan'],
+    answer: 0
+},
 
-    {
-        question: 'What is the first country in the world to grant the vote to women?',
-        alternatives: ['Australia', 'New Zealand', 'Canada'],
-        answer: 1
-    },
+{
+    question: 'What is the first country in the world to grant the vote to women?',
+    alternatives: ['Australia', 'New Zealand', 'Canada'],
+    answer: 1
+},
 
-    {
-        question: 'Which country was called Santiago when Christopher Columbus first discovered it in 1494?',
-        alternatives: ['Panama', 'Cuba', 'Jamaica'],
-        answer: 2
-    },
+{
+    question: 'Which country was called Santiago when Christopher Columbus first discovered it in 1494?',
+    alternatives: ['Panama', 'Cuba', 'Jamaica'],
+    answer: 2
+},
 
-    {
-        question: 'The Nobel Peace Prize is held every year in which country?',
-        alternatives: ['Denmark', 'Sweden', 'Norway'],
-        answer: 2
-    },
+{
+    question: 'The Nobel Peace Prize is held every year in which country?',
+    alternatives: ['Denmark', 'Sweden', 'Norway'],
+    answer: 2
+},
 
-    {
-        question: 'In which country do people say “Shalom” to greet people?',
-        alternatives: ['Israel', 'Egypt', 'Jordan'],
-        answer: 0
-    },
+{
+    question: 'In which country do people say “Shalom” to greet people?',
+    alternatives: ['Israel', 'Egypt', 'Jordan'],
+    answer: 0
+},
 
-    {
-        question: 'Which country is the current world’s leader in coffee exporting?',
-        alternatives: ['Indonesia', 'Colombia', 'Brazil'],
-        answer: 2
-    },
+{
+    question: 'Which country is the current world’s leader in coffee exporting?',
+    alternatives: ['Indonesia', 'Colombia', 'Brazil'],
+    answer: 2
+},
 
-    {
-        question: 'Which modern-day country is considered to be the successor of the Ottoman Empire?',
-        alternatives: ['Lebanon', 'Turkey', 'Jordan'],
-        answer: 1
-    },
+{
+    question: 'Which modern-day country is considered to be the successor of the Ottoman Empire?',
+    alternatives: ['Lebanon', 'Turkey', 'Jordan'],
+    answer: 1
+},
 
-    {
-        question: '“God Save the Queen” is the national anthem of which country?',
-        alternatives: ['Vatican', 'France', 'The United Kingdom'],
-        answer: 2
-    },
+{
+    question: '“God Save the Queen” is the national anthem of which country?',
+    alternatives: ['Vatican', 'France', 'The United Kingdom'],
+    answer: 2
+},
 
-    {
-        question: 'Which is the newest member of the United Nations, which declared its independence in 2011?',
-        alternatives: ['South Sudan', 'Kosovo', 'East Timor'],
-        answer: 0
-    },
+{
+    question: 'Which is the newest member of the United Nations, which declared its independence in 2011?',
+    alternatives: ['South Sudan', 'Kosovo', 'East Timor'],
+    answer: 0
+},
 
-    {
-        question: 'The Chernobyl disaster, which is considered the worst nuclear accident in history in terms of casualties and cost, occurred in 1986 in which country?',
-        alternatives: ['Ukraine', 'Russia', 'Moldova'],
-        answer: 0 
-    },
+{
+    question: 'The Chernobyl disaster, which is considered the worst nuclear accident in history in terms of casualties and cost, occurred in 1986 in which country?',
+    alternatives: ['Ukraine', 'Russia', 'Moldova'],
+    answer: 0
+},
 
-    {
-        question: 'In which country can you find the “Door to Hell”, a natural gas pit that has been on fire since 1971?',
-        alternatives: ['Georgia', 'Azerbaijan', 'Turkmenistan'],
-        answer: 2
-    },
+{
+    question: 'In which country can you find the “Door to Hell”, a natural gas pit that has been on fire since 1971?',
+    alternatives: ['Georgia', 'Azerbaijan', 'Turkmenistan'],
+    answer: 2
+},
 
-    {
-        question: 'The Strait of Gibraltar separates Spain and Portugal from which country?',
-        alternatives: ['Tunisia', 'Morocco', 'Algeria'],
-        answer: 1
-    },
+{
+    question: 'The Strait of Gibraltar separates Spain and Portugal from which country?',
+    alternatives: ['Tunisia', 'Morocco', 'Algeria'],
+    answer: 1
+},
 
-    {
-        question: 'In which of the following country do people drive their vehicles on the left of the road?',
-        alternatives: ['China', 'Estonia', 'Australia'],
-        answer: 2
-    },
+{
+    question: 'In which of the following country do people drive their vehicles on the left of the road?',
+    alternatives: ['China', 'Estonia', 'Australia'],
+    answer: 2
+},
 
-    {
-        question: 'In which country was the first national park in the world was established in 1872?',
-        alternatives: ['The United States', 'France', 'Germany'],
-        answer: 0
-    }];3
+{
+    question: 'In which country was the first national park in the world was established in 1872?',
+    alternatives: ['The United States', 'France', 'Germany'],
+    answer: 0
+}];
 
 
-    /* Start quiz */
-    
+/* Start quiz */
 
+
+/**
+ * Start quiz when user hit start
+ * Alter HTML to launch question and alternatives
+*/
 function launchQuiz() {
     let quizArea = document.getElementById('game-area');
     quizArea.innerHTML = `
 
-
-        <div class="question">
-        #question text
-        </div>
-
-        <div class="alternative-1 alternative-box">
-            #alt1     
-        </div>
-
-        <div class="alternative-2 alternative-box">
-            #alt2
-        </div>
-
-        <div class="alternative-3 alternative-box">
-            #alt3
-        </div>
-
-        <button onclick="nextQuestuon();" id="next">Next</button>
-
-        <div class="score-counter">
-            Score
-            1 / 25
-        </div>
-    `
-}
-
-
-
-
-
-
-/*
-
-    let quizContent = document.getElementsByClassName('site-content');
-
-
-    function launchQuiz() {
-    // Create div to hold gameArea
-    // let quizAreaDiv = document.createElement('div');
-    
-    let html = quizContent.innerHTML;
-    console.log(html);
-
-
-
-    quizAreaDiv.innerHTML = `
-    <div class="choice-area">
             <div class="question">
-                Which capital in Europa is highest above the sea?
+            #question text
             </div>
-            
-                <div class="alternative-1 alternative-box">
-                    Rome       
-                </div>
 
-                <div class="alternative-2 alternative-box">
-                    Paris
-                </div>
+            <button class="alt1 alt-box aria-label="Alternative 1">
+                #alt1     
+            </button>
 
-                <div class="alternative-3 alternative-box">
-                    Madrid
-                </div>
+            <button class="alt2 alt-box aria-label="Alternative 2"">
+                #alt2
+            </button>
 
-                <button onclick="nextQuestuon();" id="next">Next</button>
-            
+            <button class="alt3 alt-box aria-label="Alternative 3"">
+                #alt3
+            </button>
+
+            <button onclick="nextQuestuon();" id="next">Next</button>
+
             <div class="score-counter">
-
                 Score
                 1 / 25
             </div>
-
-            </div>
-    `;
-
-};
-
+        `
+}
 /*
-let firstQuestion = document.getElementsByClassName('gameArea')[0];
-document.body.insertBefore(quizAreaDiv, firstQuestion);
-};
+function nextQuestion () {
 
 
-    /* On next click */
-/*
-
-    next.onclick = function() {
-
-        if (if i > questionList.length -1) {
-            i=0;
-        } 
-        nextQuestion(i);
-        i++;
-    };
-*/ 
-
-
-    /* Question validation  */
-
-
-    /* Score counter */
-
-    /* */
+    
+}
+*/
